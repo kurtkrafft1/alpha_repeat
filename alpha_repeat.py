9031e3de8ac4ba74daa83f5aff41a0928e0040f4
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-14 10:19:21 -0500

    added space and fixed break issue

--- alpha_repeat.py
+++ alpha_repeat.py
@@ -1,16 +1,16 @@
 
-user_input = input("Please type a sentence, word or phrase. RULES: No Periods, No Numbers, and No Symbols. Whatcha got?")
+user_input = input("Please type a sentence, word or phrase. RULES: No Periods, No Numbers, and No Symbols. Whatcha got? ")
 
 def alpha_repeat(word_or_phrase):
     for character in word_or_phrase:
         if character.isdigit():
             print("Please no numbers")
-            break
+            return 
     for character in word_or_phrase:
         if not character.isalnum():
             if character != " ":
                 print('Please no symbols')
-                break
+                return 
     alphabet = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
     string = word_or_phrase.lower()
     print(f"okay,here we go......")
